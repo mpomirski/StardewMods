422fe277968c01b6c6061cc63555b15434af136e
mpomirski <michal.pomirski01@gmail.com>
2024-03-26 16:28:43 +0100

    First mod attempt

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using Microsoft.Xna.Framework;
-using StardewModdingAPI;
+﻿using StardewModdingAPI;
 using StardewModdingAPI.Events;
-using StardewModdingAPI.Utilities;
 using StardewValley;
 
 namespace IndoorSprinklers
@@ -17,24 +14,48 @@ namespace IndoorSprinklers
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.GameLoop.DayStarted += this.OnDayStarted;
         }
 
 
         /*********
          ** Private methods
          *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
+        
 
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        /// <summary>Raised after the game begins a new day (including when the player loads a save).</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            this.RunSprinklers();
+            this.Monitor.Log("Ran sprinklers");
+        }
+
+        private IEnumerable<GameLocation> GetIndoorLocations()
+        {
+            return Game1.locations.Where(
+                location => !location.IsOutdoors
+            );
+        }
+    
+        private void RunSprinklers()
+        {
+            foreach (var location in GetIndoorLocations())
+            {
+                foreach (var sprinkler in location.objects.Values.Where(o => o.IsSprinkler()))
+                {
+                    foreach (var sprinklerCoveredTile in sprinkler.GetSprinklerTiles())
+                    {
+                        foreach (var gameObject in location.objects.Pairs.Where(
+                                     gameObject => gameObject.Key.Equals(sprinklerCoveredTile)))
+                        {
+                            gameObject.Value.ApplySprinkler(gameObject.Key);
+                        }
+                        
+                    }
+                }
+            }
         }
     }
 }
